177bed3a52dc011ee4bd0105ea425ba2d1844589
LampartM <lampart.maciek@gmail.com>
2021-05-05 00:59:03 +0200

    JS update

--- js/main.js
+++ js/main.js
@@ -1,15 +1,13 @@
-const searchInp = document.querySelector('.search-box--input');
+const searchInput = document.querySelector('.search-box--input');
 const searchBox = document.querySelector('.search-box');
 const searchBtn = document.querySelector('.search-box--icon');
 const openMenuBtn = document.querySelector('.btn');
 const sideMenu = document.querySelector('.side-nav')
 
-console.log(sideMenu);
-
 searchBtn.addEventListener('click', openInput);
 openMenuBtn.addEventListener('click', openSideNav);
 
-function openInput() { searchInp.classList.toggle('open'); }
+function openInput() { searchInput.classList.toggle('open'); }
 
 function openSideNav() {
     sideMenu.classList.toggle('open'); 
