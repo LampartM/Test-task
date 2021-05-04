668c72dc6d9bc36d10f16c75735a6910c0d46a54
LampartM <lampart.maciek@gmail.com>
2021-05-05 00:57:54 +0200

    Small update v2

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 const searchInp = document.querySelector('.search-box--input');
 const searchBox = document.querySelector('.search-box');
 const searchBtn = document.querySelector('.search-box--icon');
-const openMenuBtn = document.querySelector('.btn-open');
+const openMenuBtn = document.querySelector('.btn');
 const sideMenu = document.querySelector('.side-nav')
 
 console.log(sideMenu);
